833da78a55f6226898500accbc46e42d141edb94
Justin Almendral <jalmendral24@gmail.com>
2020-07-13 07:52:47 -0400

    File Setting and clean-up for documentation
    Cleaned out constructor, shifted to set_text function
    Allowed set_text to handle all text setting, allowing user to totally redefine text data whenever with all three format types

--- analysis.py
+++ analysis.py
@@ -1,5 +1,5 @@
 from fileNames import text1, text2, text3, text4
-import re
+import sys
 from nltk.corpus import stopwords
 from nltk.tokenize import PunktSentenceTokenizer
 from collections import Counter
@@ -9,6 +9,22 @@ from TextWriter import write_file
 class textAnalyst():
 
     def __init__(self, user_file):
+        self.set_text(user_file)
+        
+            
+
+    stop_words = stopwords.words('english')
+    stop_words += ["like","want","know","ye","idk","i'm","really","kinda","ah","ok","oof","oh","mhm","hmm","also","lol","lmao","yeah","ooh","u"]
+    
+    def read_file(self,file_name):
+        try:
+            with open(file_name, 'r+', encoding='utf-8') as file:
+                return file.read()
+        except:
+            print("Invalid File Path")
+            sys.exit()
+
+    def set_text(self,user_file):
         if type(user_file) == list:
             if len(user_file) == 3:
                 print("Reading json file from \'" + user_file[0] + "\'")
@@ -31,47 +47,11 @@ class textAnalyst():
         else:
             print("Invalid Input.")
 
-
-                
-                
-        #if writing_list:
-         #   print("Reading json file from \'" + writing_list[0] + "\'")
-          #  print("Looking for messages from " + writing_list[1])
-           # print("Writing text to and reading text from \'" + writing_list[2] + "\'")
-            #write_file(writing_list[0],writing_list[1],writing_list[2])
-            #self.text = self.read_file(writing_list[2])
-        #if text_file:
-         #   self.text = self.read_file(text_file)
-        #if hasattr(self,'text'):
-        #    self.processed_text = self.process(self.text)
-        #if list_of_text:
-        #    compiled = [self.process(read_file(text)) for text in list_of_text]
-        #    all_sentences = list()
-        #    for file in compiled:
-        #        for sentence in file:
-        #            all_sentences.append(sentence)
-        #    self.processed_text = all_sentences
-        #    self.text = all_sentences
-
         if hasattr(self,'processed_text'):
             print("The 10 most frequent words in this text file are: ")
             for word in self.most_frequent_words(10):
                 print(word[0])
-            
-
-    stop_words = stopwords.words('english')
-    stop_words += ["like","want","know","ye","idk","i'm","really","kinda","ah","ok","oof","oh","mhm","hmm","also","lol","lmao","yeah","ooh","u"]
-    
-    def read_file(self,file_name):
-
-        try:
-            with open(file_name, 'r+', encoding='utf-8') as file:
-                return file.read()
-        except:
-            print("Invalid File Path")
-
-    def set_text(self,file_name):
-        self.text = self.read_file(file_name)
+        
         
 
     def process(self,text):
@@ -83,9 +63,9 @@ class textAnalyst():
             word_tokenized_sentences.append(word_tokenized_sentence)
         return word_tokenized_sentences
 
-    def most_frequent_words(self,num):
+    def most_frequent_words(self,n):
         all_words = [word for sentence in self.processed_text for word in sentence if word not in self.stop_words]
-        return Counter(all_words).most_common(num)
+        return Counter(all_words).most_common(n)
 
     
     def print_word(self):
