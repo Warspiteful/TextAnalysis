58243589ac06d341fb664c251a8e8e1b16a54106
Justin Almendral <jalmendral24@gmail.com>
2020-07-15 14:20:27 -0400

    Update GUI.py
    Added Favorite Part of Speech Display to Main Display
    
    Resized Display
    
    Disabled Input into Display Window

--- GUI.py
+++ GUI.py
@@ -75,7 +75,7 @@ class GUI():
             if msg[0][-4:] == 'json':
                 self.json_user_select("".join(msg))
             self.ta = gf.textAnalyst(msg)
-            self.root.geometry("500x500")
+            self.root.geometry("")
             self.reset()
             self.create_main_screen()
         except Exception:
@@ -109,7 +109,6 @@ class GUI():
                 msg += ".txt"
             self.text_path.delete(0,END)
             self.ta = gf.textAnalyst([self.json, self.users[self.v.get()-1], msg])
-            self.root.geometry("500x500")
             self.reset()
             self.create_main_screen()
 
@@ -117,33 +116,52 @@ class GUI():
 
 
     def create_main_screen(self):
-      
+        
+        pos = {"NN":"Noun","VB":"Verb","JJ":"Adjective"}
+        pos_word = []
+        for num, tag in enumerate(pos.keys()):
+            label =  Label(self.frame,text = pos[tag], padx = 20).grid(row = 0, column = num)
+            word = Text(self.frame, width = 10, height = 1, padx = 20, state = NORMAL)
+            word.grid(row = 1, column = num)
+            word.insert(END, self.ta.find_favorite_pos(tag)[0])
+            word.config(state=DISABLED)
+
         vcmd = (self.root.register(self.digit_locker))
-        self.common_words_display = Text(self.frame, bd=0, bg="white", height="25", width="20", font="Arial")
-        self.common_words_display.grid(row = 0, column = 0, rowspan = 25,  padx=10)
-        label = Label(self.frame,text = "Common Terms").grid(row = 0, column = 1)
-        button = Button(self.frame, font=("Verdana",12,'bold'), text="Send", width="12", height=1,
-                        bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
-                        command= self.most_common).grid(row = 2, column = 1)
+        self.common_words_display = Text(self.frame, bd=0, bg="white", height="10", width="10", font="Arial")
+        self.common_words_display.grid(row = 5, column = 0, rowspan = 10,  padx=10)
+        label = Label(self.frame,text = "Common Terms").grid(row = 2, column = 0)
         self.common_words_entry = Entry(self.frame, bd=0, bg="white", width="10", font="Arial", validate = "all", validatecommand = (vcmd,'%P'))
-        self.common_words_entry.grid(row = 1, column = 1)
+        self.common_words_entry.grid(row = 3, column = 0)
+        button = Button(self.frame, font=("Verdana",12,'bold'), text="Send", width="8", height=1,
+                        bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
+                        command= self.most_common).grid(row = 4, column = 0)
+        self.common_words_display.config(state = DISABLED)
+     
+
+
+          
+        
+       
+
         
         
     
         self.compile()
 
     def digit_locker(self, P):
-        if str.isdigit(P):
+        if str.isdigit(P) or P == '':
             return True
         else:
             return False
 
     def most_common(self):
+        self.common_words_display.config(state = NORMAL)
         self.common_words_display.delete('1.0', END)
         if int(self.common_words_entry.get()) > 0:
             words = self.ta.most_frequent_words(int(self.common_words_entry.get()))
             for num, word in enumerate(words):
                 self.common_words_display.insert(END, str(num + 1) + ". " + word[0] + "\n")
+        self.common_words_display.config(state = DISABLED)
 
     def reset(self):
         if hasattr(self,'frame'):
