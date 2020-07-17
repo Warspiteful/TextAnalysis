51315848e9d3c7e8cf201be4e916a8f25f9fe058
Justin Almendral <jalmendral24@gmail.com>
2020-07-17 18:17:06 -0400

    Update textAnalysis.py
    Created function to allow reading of non-cleaned text files (text files not already cleaned as a part of generatation from JSON)

--- textAnalysis.py
+++ textAnalysis.py
@@ -11,6 +11,26 @@ class textAnalyst():
     text = ""
     processed_text = []
     text_fields = [text,processed_text]
+    emoji_pattern = re.compile("["
+            u"\U0001F600-\U0001F64F"  # emoticons
+            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+            u"\U0001F680-\U0001F6FF"  # transport & map symbols
+            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+            u"\U0001F1F2-\U0001F1F4"  # Macau flag
+            u"\U0001F1E6-\U0001F1FF"  # flags
+            u"\U0001F600-\U0001F64F"
+            u"\U00002702-\U000027B0"
+            u"\U000024C2-\U0001F251"
+            u"\U0001f926-\U0001f937"
+            u"\U0001F1F2"
+            u"\U0001F1F4"
+            u"\U0001F620"
+            u"\u200d"
+            u"\u2640-\u2642"
+            u"\uFFF0-\uFFFF"
+            
+            "]+", flags=re.UNICODE)
+
 
     def __init__(self, user_file):
         self.set_stopwords()
@@ -49,8 +69,7 @@ class textAnalyst():
             print(ntpath.basename(file))
     
 
-    def remove_text_file(self,file_name):
-        
+    def remove_text_file(self,file_name):    
         for file in enumerate(self.text_files):
             if file_name in ntpath.basename(file[1]):    
                 print("\nRemoving " + ntpath.basename(file_name) + " from the model.")
@@ -103,7 +122,9 @@ class textAnalyst():
             else:
                 print("\nReading from:\n" + "\n".join([ntpath.basename(file) for file in user_file]))
                 text = [self.read_file(text) for text in user_file]
+                
                 self.text += "".join(text)
+                self.text = self.cleanseText(self.text)
                 compiled = [self.process(texts) for texts in text]
                 all_sentences = list()
                 for file in compiled:
@@ -159,41 +180,28 @@ class textAnalyst():
         data = data["messages"]
         texts = []
 
-        emoji_pattern = re.compile("["
-            u"\U0001F600-\U0001F64F"  # emoticons
-            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-            u"\U0001F680-\U0001F6FF"  # transport & map symbols
-            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-            u"\U0001F1F2-\U0001F1F4"  # Macau flag
-            u"\U0001F1E6-\U0001F1FF"  # flags
-            u"\U0001F600-\U0001F64F"
-            u"\U00002702-\U000027B0"
-            u"\U000024C2-\U0001F251"
-            u"\U0001f926-\U0001f937"
-            u"\U0001F1F2"
-            u"\U0001F1F4"
-            u"\U0001F620"
-            u"\u200d"
-            u"\u2640-\u2642"
-            u"\uFFF0-\uFFFF"
-            
-            "]+", flags=re.UNICODE)
-
+        
 
         for message in data:
             if message.get('from') == name:
                 if type(message.get('text')) is not list:
                     texts.append(message.get('text'))
         for text in texts:
-            text = emoji_pattern.sub(r'',text)
+            text = self.emoji_pattern.sub(r'',text)
             allchars = [str for str in text]
             emoji_list = [c for c in allchars if c in emoji.UNICODE_EMOJI]
             clean_text = ' '.join([str for str in text.split() if not any(i in str for i in emoji_list)])
             clean_text = clean_text.encode('utf-8').strip().decode()
             clean_text.strip("~")
-            clean_text = emoji_pattern.sub(r'',clean_text)
+            clean_text = self.emoji_pattern.sub(r'',clean_text)
             clean_text = re.sub(r'[^\x00-\x7F]+',' ', clean_text)
             writer.write(str((clean_text)))
             writer.write('\n')
         writer.close()
 
+    def cleanseText(self, text):
+        clean_text = text.encode('utf-8').strip().decode()
+        clean_text.strip("~")
+        clean_text = self.emoji_pattern.sub(r'',clean_text)
+        clean_text = re.sub(r'[^\x00-\x7F]+',' ', clean_text)
+        return clean_text
